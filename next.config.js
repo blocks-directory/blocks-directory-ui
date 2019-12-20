139aed53d1c6853794ab9c510d785d82db101239
Arseny Yankovsky <arseny.yankovsky@emarketeer.com>
2019-12-21 00:53:00 +0100

    WIUP

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const withTM = require('next-transpile-modules')
 
 module.exports = withTM({
+  target: 'serverless',
   transpileModules: ['lodash-es'],
   devIndicators: {
     autoPrerender: false,
