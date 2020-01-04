3063bea7ef8dc04a26a891715f0ebaba1a2cc46d
ayankovsky <p44gr10@gmail.com>
2020-01-04 14:32:06 +0100

    Initial blog implementation

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'styled-components'
 
 import { rhythm, scale } from '../utils/typography'
 import { Bio, Layout, Seo, StyledLink } from '../components/blog'
+
+const Date = styled.p`
+  font-size: ${scale(-1 / 5).fontSize};
+  line-height: ${scale(-1 / 5).lineHeight};
+  display: block;
+  margin-bottom: ${rhythm(1)};
+`
+const PostTitle = styled.h1`
+  margin-top: ${rhythm(1)};
+  margin-bottom: 0;
+`
+const Spacer = styled.hr`
+  margin-bottom: ${rhythm(1)};
+`
+const NavigationWrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 0;
+  justify-content: space-between;
+  list-style: none;
+  padding: 0;
+`
 
 const BlogPostTemplate = (props: any) => {
   const { pageContext: { previous, next }, data } = props
@@ -14,55 +37,31 @@ const BlogPostTemplate = (props: any) => {
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
       />
-      <h1
-        style={{
-          marginTop: rhythm(1),
-          marginBottom: 0,
-        }}
-      >
+      <PostTitle>
         {post.frontmatter.title}
-      </h1>
-      <p
-        style={{
-          ...scale(-1 / 5),
-          display: 'block',
-          marginBottom: rhythm(1),
-        }}
-      >
+      </PostTitle>
+      <Date>
         {post.frontmatter.date}
-      </p>
+      </Date>
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      <hr
-        style={{
-          marginBottom: rhythm(1),
-        }}
-      />
+      <Spacer />
       <Bio />
-      <ul
-        style={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          marginLeft: 0,
-          justifyContent: 'space-between',
-          listStyle: 'none',
-          padding: 0,
-        }}
-      >
+      <NavigationWrapper>
         <li>
           {previous && (
-            <StyledLink to={previous.fields.slug} rel="prev">
+            <StyledLink to={`/blog/${previous.fields.slug}`} rel="prev">
               ← {previous.frontmatter.title}
             </StyledLink>
           )}
         </li>
         <li>
           {next && (
-            <StyledLink to={next.fields.slug} rel="next">
+            <StyledLink to={`/blog/${next.fields.slug}`} rel="next">
               {next.frontmatter.title} →
             </StyledLink>
           )}
         </li>
-      </ul>
+      </NavigationWrapper>
     </Layout>
   )
 }
