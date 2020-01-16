1635e19258fd033259e87d8be7de6c35dddc2f13
Eugene Draitsev <ddrrai@gmail.com>
2020-01-16 23:14:15 +0100

    Added md viewer and some styles

---
+++ src/components/md-viewer.component.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+import { useAsync } from 'react-use'
+import ReactMarkdown from 'react-markdown'
+import { CircularProgress } from '@material-ui/core'
+
+type MdViewerProps = {
+  url: string
+}
+
+const Wrapper = styled.div`
+  margin: 10px 0;
+`
+
+export const MdViewer = ({ url }: MdViewerProps) => {
+  const { loading, value, error } = useAsync(async () => {
+    const response = await fetch(url)
+    const result = await response.text()
+    return result
+  }, [url])
+
+
+  if (loading) {
+    return (
+      <Wrapper>
+        <CircularProgress />
+      </Wrapper>
+    )
+  }
+
+  if (error) {
+    return <Wrapper>Can&apos;t load ReadMe</Wrapper>
+  }
+
+  return (
+    <Wrapper>
+      <ReactMarkdown source={value} />
+    </Wrapper>
+  )
+}
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -2,26 +2,26 @@ import React from 'react'
 import styled from 'styled-components'
 import { Router } from '@reach/router'
 import { useQuery } from '@apollo/react-hooks'
-import { Chip, Typography } from '@material-ui/core'
+import { Box, Chip, Typography } from '@material-ui/core'
 import { Skeleton } from '@material-ui/lab'
 import { DateTime } from 'luxon'
 import { ExternalLink } from 'react-feather'
 import { capitalize, upperCase } from 'lodash-es'
 
-import { AppBarLayout } from '../components'
+import { AppBarLayout, MdViewer } from '../components'
 import { getProjectById as Project } from '../graphql/queries/types/getProjectById'
 import { getProjectById } from '../graphql/queries'
 
 const Wrapper = styled.div`
   padding: 20px 0;
-`
+  `
 const Title = styled(Typography)`
   color: black;
   font-weight: bold;
   font-size: 18px;
   line-height: 22px;
   margin-bottom: 10px;
-`
+  `
 const Tag = styled(Chip)`
   margin-right: 10px;
   font-size: 16px;
@@ -32,6 +32,19 @@ const Tag = styled(Chip)`
   .MuiChip-label {
     padding: 0 18px;
   }
+  `
+const MetaInfo = styled.div`
+  display: grid;
+  grid-template-columns: 80px 150px 100px 100px;
+  grid-gap: 15px;
+  margin-top: 20px;
+`
+const InfoTitle = styled.div`
+  margin-right: 10px;
+`
+const ExternalLinkIcon = styled(ExternalLink)`
+  max-width: 15px;
+  max-height: 15px;
 `
 
 const ProjectPage = ({ projectId }: any) => {
@@ -42,8 +55,6 @@ const ProjectPage = ({ projectId }: any) => {
     name, lastCommitDate, runtime, provider, readmeUrl, platform, repositoryUrl, openIssues, pullRequests,
   } = data?.getProjectById ?? {} as any
 
-  console.log(data?.getProjectById)
-
   return (
     <AppBarLayout title="">
       <Wrapper>
@@ -51,8 +62,9 @@ const ProjectPage = ({ projectId }: any) => {
           <>
             <Skeleton animation="wave" width={210} />
             <Skeleton animation="wave" width={210} />
-            <Skeleton animation="wave" width={210} />
-            <Skeleton animation="wave" width={210} />
+            <Box mt={2}>
+              <Skeleton animation="wave" width={470} height={170} variant="rect" />
+            </Box>
           </>
         )}
         {!loading && (
@@ -62,18 +74,28 @@ const ProjectPage = ({ projectId }: any) => {
               Last commit {DateTime.fromISO(lastCommitDate).toRelative()}
             </Typography>
 
-            <div>
-              Type: <Tag label={capitalize(platform)} />
-              Repository: <a href={repositoryUrl} rel="noopener noreferrer" target="_blank">github <ExternalLink /></a>
-            </div>
-            <div>
-              Runtime: <Tag label={capitalize(runtime)} />
-              Open issues: {openIssues}
-            </div>
-            <div>
-              Provider: <Tag label={upperCase(provider)} />
-              Pull requests: {pullRequests}
-            </div>
+            <MetaInfo>
+              <InfoTitle>Type</InfoTitle>
+              <Tag label={capitalize(platform)} />
+
+              <InfoTitle>Repository</InfoTitle>
+              <a href={repositoryUrl} rel="noopener noreferrer" target="_blank">
+                github <ExternalLinkIcon />
+              </a>
+
+              <InfoTitle>Runtime</InfoTitle>
+              <div><Tag label={capitalize(runtime)} /></div>
+
+              <InfoTitle>Open issues </InfoTitle>
+              {openIssues}
+
+              <InfoTitle>Provider </InfoTitle>
+              <div><Tag label={upperCase(provider)} /></div>
+
+              <InfoTitle>Pull requests </InfoTitle>
+              {pullRequests}
+            </MetaInfo>
+            <MdViewer url={readmeUrl} />
           </div>
         )}
       </Wrapper>
