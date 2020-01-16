02124cd6c81fb37a65d449113b37e1e1a67aee79
Eugene Draitsev <ddrrai@gmail.com>
2020-01-16 23:17:41 +0100

    Added missed type

---
+++ src/graphql/mutations/types/submitProject.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: submitProject
+// ====================================================
+
+export interface submitProject_submitProject {
+  __typename: "SubmitProjectResponse";
+  repositoryUrl: string;
+}
+
+export interface submitProject {
+  submitProject: submitProject_submitProject;
+}
+
+export interface submitProjectVariables {
+  repositoryUrl: string;
+}
